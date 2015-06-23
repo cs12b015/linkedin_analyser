52f859d8ad5feb496363b46187d6e259a0ea6396
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-06-23 12:25:34 +0530

    working with dbs

--- app.js
+++ app.js
@@ -18,15 +18,21 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var graph = require('fbgraph');
 
 var mongoose= require('mongoose');
-
+mongoose.connect('mongodb://localhost/newdb');
 
 
 var app = express();
 var linkedin;
-var linkedin_edu=[];
-var linkedin_work=[];
-var fb_work=[];
-var fb_edu=[];
+var linkedin_edu1=[];
+var linkedin_work1=[];
+var fb_work1=[];
+var fb_edu1=[];
+
+
+var linkedin_work=mongoose.model('linkedin_work',{name:String,id:String},'linkedin_work');
+var linkedin_edu=mongoose.model('linkedin_edu',{name:String,id:Number},'linkedin_edu');
+var facebook_work=mongoose.model('facebook_work',{name:String,id:Number},'facebook_work');
+var facebook_edu=mongoose.model('facebook_edu',{name:String,id:Number},'facebook_edu');
 
 
 passport.use(new FacebookStrategy({
@@ -44,26 +50,56 @@ function(accessToken, refreshToken, profile, done) {
 
 
 
-var scrapedata=function(MAINURL){
+var scrapedata=function(MAINURL,linid){
   request(MAINURL, function (error, response, body)
   {
     if (!error && response.statusCode == 200)
-      {
-        $ = cheerio.load(body);
-        $('.editable-item.section-item.current-position').find('h5 a').each(function(index,item)
-        { 
-          var boss= $(item).text();
-          if(boss!="")
-          linkedin_work[linkedin_work.length]=boss;
+    {
+      $ = cheerio.load(body);
+      $('.editable-item.section-item.current-position').find('h5 a').each(function(index,item)
+      { 
+        var boss= $(item).text();
+        if(boss!=""){
+          linkedin_work1[linkedin_work1.length]=boss;
+          linkedin_work.count({name:boss,id:linid},function(err,count){
+            if(!count){
+                var inworkitem= new linkedin_work({name:boss,id:linid});
+                inworkitem.save(function (err) {
+                  if (err) throw err;
+                  else
+                  console.log('inserted');
+                });
+              }
+              else{
+                console.log('Already Exists')
+              }
+          });
+
+
+        }        
       });
       $ = cheerio.load(body);
-        $('.editable-item.section-item.past-position').find('h5 a').each(function(index,item)
-        { 
-          var boss= $(item).text();
-          if(boss!="")
-          linkedin_work[linkedin_work.length]=boss;
+      $('.editable-item.section-item.past-position').find('h5 a').each(function(index,item)
+      { 
+        var boss= $(item).text();
+        if(boss!=""){
+          linkedin_work1[linkedin_work1.length]=boss;
+          linkedin_work.count({name:boss,id:linid},function(err,count){
+            if(!count){
+                var inworkitem= new linkedin_work({name:boss,id:linid});
+                inworkitem.save(function (err) {
+                  if (err) throw err;
+                  else
+                  console.log('inserted');
+                });
+              }
+              else{
+                console.log('Already Exists')
+              }
+          });
+        }
       });
-      console.log(linkedin_work);
+      console.log(linkedin_work1);
     }
   });
 }
@@ -101,25 +137,73 @@ app.get('/start',function(req,res,next){
 app.get('/inhome',function(req,res,next){ 
   linkedin.people.me(function(err, $in) {
     var mainurl =$in.publicProfileUrl
-      scrapedata(mainurl);
-      //console.log($in);  
+    var linid= $in.id;
+      scrapedata(mainurl,linid);
+      console.log($in);  
   });
   res.redirect('/auth/facebook');
 });
 
+var putinfbedu=function(data2,papid){
+  facebook_edu.count(data2,function(err,count){
+    if(!count){
+      var fbeduitem= new facebook_edu(data2);
+      fbeduitem.save(function (err) {
+        if (err) throw err;
+        else
+        console.log('fb edu inserted');
+      });
+    }
+    else{
+      console.log('Already Exists fb edu')
+    }
+  });
+}
+
+var putinfbwork=function(data1,papid){
+  facebook_work.count(data1,function(err,count){
+    if(!count){
+      var fbworkitem= new facebook_work(data1);
+      fbworkitem.save(function (err) {
+        if (err) throw err;
+        else
+        console.log('fb work inserted');
+      });
+    }
+    else{
+      console.log('Already Exists fb work')
+    }
+  });
+}
+
+
+
+
 app.get('/fbhome',function(req,res,next){ 
   graph.get('me?fields=education,work', function(err, res) {
+   // console.log(res);
     var edu=res.education;
     var workk=res.work;
+    var papid= res.id;
+    
     for(var i=0;i<edu.length;i++){
-      fb_edu[i]=edu[i].school.name;
-    }
-    for(var i=0;i<workk.length;i++){
-      fb_work[i]=workk[i].employer.name;
+      fb_edu1[i]=edu[i].school.name;
+      var pap =edu[i].school.name;
+      var data2= {name:pap,id:papid}
+      putinfbedu(data2,papid);
     }
 
-    console.log(fb_edu);
-    console.log(fb_work);
+
+
+    for(var i=0;i<workk.length;i++){
+      fb_work1[i]=workk[i].employer.name;
+      var pat=workk[i].employer.name;
+      var data1={name:pat,id:papid};
+      putinfbwork(data1,papid);
+    }
+
+    console.log(fb_edu1);
+    console.log(fb_work1);
   });
   res.redirect('/');
 });
